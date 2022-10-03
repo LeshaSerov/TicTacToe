9fab6f1387b8d1559f25d7a488a149293d4a001a
LeshaSerov <93645965+LeshaSerov@users.noreply.github.com>
2022-10-03 12:26:06 +0400

    Initial commit

---
+++ TicTacToe/Field.cs
@@ -0,0 +1,64 @@
+﻿namespace TicTacToe;
+
+public class Field
+{
+    private char[,] _box = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+
+    public bool AddWalk(int i, int j, char walk)
+    {
+        try
+        {
+            if (_box[i - 1, j - 1] == ' ')
+                _box[i - 1, j - 1] = walk;
+            else
+                return false;
+        }
+        catch (IndexOutOfRangeException)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public void DrawField()
+    {
+        // Console.WriteLine("-1-2-3-");
+        Console.WriteLine(_box[0, 0] + "|" + _box[0, 1] + "|" + _box[0, 2]);
+        Console.WriteLine("------");
+        Console.WriteLine(_box[1, 0] + "|" + _box[1, 1] + "|" + _box[1, 2]);
+        Console.WriteLine("------");
+        Console.WriteLine(_box[2, 0] + "|" + _box[2, 1] + "|" + _box[2, 2]);
+    }
+
+    public bool ItHaveEmptyBox()
+    {
+        int emptyBoxs = 0;
+        foreach (char c in _box)
+        {
+            if (c == ' ')
+                emptyBoxs++;
+        }
+        return emptyBoxs == 0;
+    }
+
+    public bool CheckWin()
+    {
+        if (
+            (_box[0, 0] != ' ' && _box[0, 0] == _box[0, 1] && _box[0, 0] == _box[0, 2])
+            || (_box[0, 0] != ' ' && _box[0, 0] == _box[1, 0] && _box[0, 0] == _box[2, 0])
+            || (_box[0, 0] != ' ' && _box[0, 0] == _box[1, 1] && _box[0, 0] == _box[2, 2])
+            || (_box[2, 0] != ' ' && _box[2, 0] == _box[1, 1] && _box[2, 0] == _box[0, 2])
+            || (_box[2, 2] != ' ' && _box[2, 2] == _box[2, 1] && _box[2, 2] == _box[2, 0])
+            || (_box[2, 2] != ' ' && _box[2, 2] == _box[1, 2] && _box[2, 2] == _box[0, 2])
+            || (_box[0, 1] != ' ' && _box[0, 1] == _box[1, 1] && _box[0, 1] == _box[2, 1])
+            || (_box[1, 0] != ' ' && _box[1, 0] == _box[1, 1] && _box[1, 0] == _box[1, 2])
+        )
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace TicTacToe
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Field field = new Field();
+            bool moveX = true;
+
+            while (field.ItHaveEmptyBox() && !field.CheckWin())
+            {
+                Console.WriteLine("Игровое поле:");
+                field.DrawField();
+                Console.WriteLine((moveX ? "Ход крестиков" : "Ход ноликов") + ", введите координаты через запятую:");
+                string? str = Console.ReadLine();
+
+                try
+                {
+                    string[] split = str.Split(",");
+                    int i = int.Parse(split[0]);
+                    int j = int.Parse(split[1]);
+                    char c = moveX ? 'x' : 'o';
+
+                    if (field.AddWalk(i, j, c))
+                    {
+                        moveX ^= true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введены не корректные координаты");
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Неверное количество координат");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неверный формат координат");
+                }
+
+                Console.WriteLine("");
+            }
+            if (field.ItHaveEmptyBox())
+                Console.WriteLine("Победили " + (!moveX ? "крестики" : "нолики") + '!');
+            else
+                Console.WriteLine("Ничья");
+            field.DrawField();
+        }
+    }
+}
